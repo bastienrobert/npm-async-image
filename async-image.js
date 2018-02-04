30fc142de9a0f80c5bd85105019fabea070e4ed5
Bastien <bastienrbt@protonmail.com>
2018-02-04 18:35:52 +0100

    Add assets

---
+++ async-image.js
@@ -0,0 +1,30 @@
+class AsyncImage {
+  constructor () {
+    this.els = document.querySelectorAll("[data-compress]")
+    this.engine()
+  }
+  engine () {
+  	for (var i = 0; i < this.els.length; i++) {
+      var el = this.els[i]
+      var d = el.src
+      this.small(el)
+      this.load(el, d)
+    }
+  }
+  load (el, d) {
+    var img = new Image()
+    img.src = d
+    img.onload = function () {
+      el.src = img.src
+      el.classList.add('async-large-loaded')
+      el.classList.remove('async-small-loaded')
+    }
+  }
+  small (el) {
+    el.src = el.dataset.compress
+    el.classList.add('async-small-loaded')
+    delete el.dataset.compress
+  }
+}
+
+export default new asyncImage
